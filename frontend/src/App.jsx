b108833da46acc966f8f396732afc6ea6489d7c9
dinesh-bazgain <dinesh.bazgain@gmail.com>
2025-06-26 18:30:58 +0545

    imported pages in the app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './app.css'
+import HomePage from './pages/HomePage'
+import ProfilePage from './pages/ProfilePage'
+import LoginPage from './pages/LoginPage'
 
 const App = () => {
   return (
     <div>
-      
+      <HomePage/>
+      <LoginPage/>
+      <ProfilePage/>
     </div>
   )
 }
