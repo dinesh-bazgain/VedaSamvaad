ded4614a3f5fee11e28231c3dadf50e0ee14ad28
dinesh-bazgain <dinesh.bazgain@gmail.com>
2025-06-29 23:07:20 +0545

    Profile page content cahnged

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const ProfilePage = () => {
   return (
     <div>
-      
+      <h1>Profile</h1>
     </div>
-  )
-}
+  );
+};
 
-export default ProfilePage 
+export default ProfilePage;
