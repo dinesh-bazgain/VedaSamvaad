805bf9057f9df38be8a72dd2f51f5ef74064b497
dinesh-bazgain <dinesh.bazgain@gmail.com>
2025-07-27 23:49:47 +0545

    change in the homepage styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import LoginPage from "./pages/LoginPage";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-400 bg-[url('./public/background.png')] bg-no-repeat bg-center bg-[length:200px_200px] ">
+    <div className="min-h-screen bg-gray-400 bg-[url('./public/background.png')] bg-no-repeat bg-center bg-[length:300px_300px]">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
