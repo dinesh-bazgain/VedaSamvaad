32de155747198cc0972b07cd2c6323807df763ce
dinesh-bazgain <dinesh.bazgain@gmail.com>
2025-07-29 18:06:40 +0545

    created controller for user login and auth, created a connection to the database

---
+++ backend/controllers/userController.js
@@ -0,0 +1,59 @@
+import { generateToken } from "../lib/utils";
+import User from "../models/User";
+import bcrypt from "bcryptjs";
+
+// Sign up new user
+export const signUp = async (req, res) => {
+  const { fullName, email, password, profilePic, bio } = req.body;
+  try {
+    if (!fullName || !email || !password) {
+      return res.json({ success: false, message: "All fields are required" });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = await User.create({
+      fullName,
+      email,
+      password: hashedPassword,
+      bio,
+    });
+    const token = generateToken(newUser._id);
+    res.json({
+      success: true,
+      userData: newUser,
+      token,
+      message: "User created successfully",
+    });
+  } catch (error) {
+    res.json({ success: false, message: "Server error", error: error.message });
+  }
+};
+
+// controller to handle user login
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userData = await User.findOne({ email });
+    const isPasswordCorrect = await bcrypt.compare(password, userData.password);
+    if (!isPasswordCorrect){
+        return res.json({ success: false, message: "Invalid credentials" });
+    }
+    const token = generateToken(userData._id);
+    res.json({
+      success: true,
+      userData,
+      token,
+      message: "Login successful",
+    });
+
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: "Server error", error: error.message });
+  }
+};
+
+
