2d94397a7be77782dfe47ed967b425ea7e360c99
dinesh-bazgain <dinesh.bazgain@gmail.com>
2025-07-31 00:43:28 +0545

    fixed the routes for the deletion process

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,8 @@ import express from "express";
 import { use } from "react";
 import { checkAuth, login, signUp, updateProfile } from "../controllers/userController.js";
 import { protectRoute } from "../middleware/auth.js";
+import { deleteAccount } from "../controllers/userController.js";
+
 
 const userRouter = express.Router();
 
@@ -9,5 +11,7 @@ userRouter.post("/signup", signUp);
 userRouter.post("/login", login);
 userRouter.put("/update-profile", protectRoute, updateProfile);
 userRouter.get("/check", protectRoute, checkAuth);
+userRouter.delete('/delete-account', protectRoute, deleteAccount);
+
 
 export default userRouter;
