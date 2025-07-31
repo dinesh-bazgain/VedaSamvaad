fbf29842c9c4cc92c0d87754567505426a79a776
dinesh-bazgain <dinesh.bazgain@gmail.com>
2025-07-31 17:25:57 +0545

    removed the react import from the userRout.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { use } from "react";
-import { checkAuth, login, signUp, updateProfile } from "../controllers/userController.js";
+import {
+  checkAuth,
+  login,
+  signUp,
+  updateProfile,
+} from "../controllers/userController.js";
 import { protectRoute } from "../middleware/auth.js";
 import { deleteAccount } from "../controllers/userController.js";
-
 
 const userRouter = express.Router();
 
@@ -11,7 +14,6 @@ userRouter.post("/signup", signUp);
 userRouter.post("/login", login);
 userRouter.put("/update-profile", protectRoute, updateProfile);
 userRouter.get("/check", protectRoute, checkAuth);
-userRouter.delete('/delete-account', protectRoute, deleteAccount);
-
+userRouter.delete("/delete-account", protectRoute, deleteAccount);
 
 export default userRouter;
