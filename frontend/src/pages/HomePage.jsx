2f621e91de8c839759fdd7a795dd898d9a846110
dinesh-bazgain <dinesh.bazgain@gmail.com>
2025-08-02 11:54:26 +0545

    removed the trash

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,14 +10,11 @@ const HomePage = () => {
     <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
       <div
         className={`backdrop-blur-sm border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${
-          selectedUser
-            ? "md:grid-cols-[1fr_2fr]" // Removed the third column
-            : "md:grid-cols-2"
+          selectedUser ? "md:grid-cols-[1fr_2fr]" : "md:grid-cols-2"
         }`}
       >
         <Sidebar />
         <ChatContainer />
-        {/* Removed RightSidebar component from here */}
       </div>
     </div>
   );
